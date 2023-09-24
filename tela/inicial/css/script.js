5f3e5b0c3283e235dc2ce79df0baef19ced95e92
luffyelbrabo <106891190+luffyelbrabo@users.noreply.github.com>
2023-09-24 01:08:07 -0300

    Update script.js

--- tela/inicial/css/script.js
+++ tela/inicial/css/script.js
@@ -1,3 +1,25 @@
+
+function openUrl(url) {
+    window.open(url, '_blank');
+}
+
+function closeOverlay() {
+    document.querySelector('.overlay-tab').style.left = '-50%';
+}
+
+// Função para abrir a aba de sobreposição
+function openOverlay() {
+    document.querySelector('.overlay-tab').style.left = '0';
+}
+
+// Chame a função openOverlay() quando desejar abrir a aba (por exemplo, quando o site é carregado)
+openOverlay();
+
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
     const menuButton = document.getElementById("menu-button");
     const menuOptions = document.getElementById("menu-options");
