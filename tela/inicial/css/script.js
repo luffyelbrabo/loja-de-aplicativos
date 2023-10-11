ad0aba7a05b778096c2c23e9f0c97e9283d63e86
luffyelbrabo <106891190+luffyelbrabo@users.noreply.github.com>
2023-10-11 18:24:02 -0300

    Add files via upload

--- tela/inicial/css/script.js
+++ tela/inicial/css/script.js
@@ -34,11 +34,3 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-document.getElementById('menu-button').addEventListener('click', function() {
-    var menu = document.getElementById('menu');
-    if (menu.style.display === 'none' || menu.style.display === '') {
-        menu.style.display = 'block';
-    } else {
-        menu.style.display = 'none';
-    }
-});
